bc993ed5354e787f424a495473d0d661ae9801f3
OLGON92 <ogon318@gmail.com>
2023-03-19 16:47:00 -0700

    create method to handle form submission

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -13,17 +13,24 @@ class TicketControl extends React.Component {
     };
   }
 
-  handleClick = () => {
+  handleAddingNewTicketToLIst = (newTicket) => {
+    const newMainTicketList = this.state.mainTicketList.concat(newTicket);
+    this.setState({mainTicketList: newMainTicketList,
+                  formVisibleOnPage: false});
+  }
+  /*handleClick = () => {
     this.setState(prevState => ({
       formVisibleOnPage: !prevState.formVisibleOnPage
     }));
-  }
+  }*/
+
 
   render(){
     let currentlyVisibleState = null;
     let buttonText = null;
     if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewTicketForm />
+      currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />
+      //we prefix the prop with on. This differentiates the method in our parent component (which will actually handle the event) from the function in our child component (which is triggered when the event happens).
       buttonText = "Return to Ticket List";
     } else {
       currentlyVisibleState = <TicketList ticketList={this.state.mainTicketList} />; //we're passing mainTicketList down to TicketList
