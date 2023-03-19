3c64b81b22c2427fec08d8d23eebcd3f6916993b
OLGON92 <ogon318@gmail.com>
2023-03-19 14:35:10 -0700

    add commented out code, and some console.logs

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 function NewTicketForm() {
+
+  function handleNewTicketFormSubmission(event) {
+    event.preventDefault();
+    console.log(event.target.names.value);
+    console.log(event.target.location.value);
+    console.log(event.target.issue.value);
+  }
   return (
     <React.Fragment>
       <form onSubmit={handleNewTicketFormSubmission}>
@@ -22,3 +29,9 @@ function NewTicketForm() {
 }
 
 export default NewTicketForm;
+
+/*First, we will create a method called onNewTicketCreation in the parent TicketControl component.
+Next, we'll pass the onNewTicketCreation function to its child NewTicketForm component as a prop.
+Our child NewTicketForm component has a function called handleNewTicketFormSubmission which correctly gathers user inputs from a form. We will add onNewTicketCreation to the handleNewTicketFormSubmission as a callback.
+Form data will be passed into the onNewTicketCreation callback via its parameters.
+The parent TicketControl component will have access to that data, which it can then use to add a new ticket to our mainTicketList.*/
