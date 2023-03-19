8b04415d6bcf1a44ef852881623a8b3ea7772e32
OLGON92 <ogon318@gmail.com>
2023-03-19 14:50:37 -0700

    move mainTicketLIst into State

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -3,11 +3,13 @@ import NewTicketForm from "./NewTicketForm";
 import TicketList from "./TicketList";
 
 class TicketControl extends React.Component {
-
+  
+  //adding a mainTicketList property to state and passing it down as a prop to TicketList
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      formVisibleOnPage: false,
+      mainTicketList: [] //The queue should be empty until we start adding tickets via our form; no fake tickets
     };
   }
 
@@ -24,7 +26,8 @@ class TicketControl extends React.Component {
       currentlyVisibleState = <NewTicketForm />
       buttonText = "Return to Ticket List";
     } else {
-      currentlyVisibleState = <TicketList />
+      currentlyVisibleState = <TicketList ticketList={this.state.mainTicketList} />; //we're passing mainTicketList down to TicketList
+      //Here we're calling it ticketList, so that's the name we'll use to access it as a prop in TicketList.
       buttonText = "Add Ticket";
     }
     return (
